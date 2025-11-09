323eb5387a94bca6965a03e085ab8d1a6647c5a5
jenithjain <jenithjain09@gmail.com>
2025-11-09 07:31:41 +0530

    delete

--- test-env.js
+++
@@ -1,6 +0,0 @@
-// Test script to verify environment variables
-import 'dotenv/config';
-
-console.log('DATABASE_URL from env:', process.env.DATABASE_URL);
-console.log('DATABASE_URL length:', process.env.DATABASE_URL?.length);
-console.log('Contains encoded chars:', process.env.DATABASE_URL?.includes('%24'));
